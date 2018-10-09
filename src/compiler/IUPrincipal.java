54b2ffd7f049d703492f197ca79f3ec680c31177
Guilherme <Guilherme@192.168.1.8>
2018-10-09 19:10:56 -0300

    Init Sintatic

--- src/compiler/IUPrincipal.java
+++ src/compiler/IUPrincipal.java
@@ -47,6 +47,9 @@ public class IUPrincipal extends javax.swing.JFrame {
         TableLexico = new javax.swing.JTable();
         jScrollPane1 = new javax.swing.JScrollPane();
         TextLanguage = new javax.swing.JTextPane();
+        jPanel2 = new javax.swing.JPanel();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        TabelaSintatico = new javax.swing.JTable();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenu2 = new javax.swing.JMenu();
@@ -102,6 +105,35 @@ public class IUPrincipal extends javax.swing.JFrame {
 
         jScrollPane1.setViewportView(TextLanguage);
 
+        jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder("Analisador Sintático"));
+
+        TabelaSintatico.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Informacao"
+            }
+        ));
+        jScrollPane3.setViewportView(TabelaSintatico);
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 131, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(307, 307, 307))
+        );
+
         jMenuBar1.setBorder(null);
 
         jMenu1.setText("Abrir");
@@ -139,12 +171,13 @@ public class IUPrincipal extends javax.swing.JFrame {
                         .addComponent(jButton1))
                     .addComponent(jScrollPane1)
                     .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(10, 10, 10)
-                                .addComponent(jLabel1)))
-                        .addGap(0, 198, Short.MAX_VALUE)))
+                        .addGap(10, 10, 10)
+                        .addComponent(jLabel1)
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -158,8 +191,10 @@ public class IUPrincipal extends javax.swing.JFrame {
                 .addComponent(jButton1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 170, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(14, 14, 14)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 170, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
@@ -168,20 +203,25 @@ public class IUPrincipal extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         ArrayList<Token> tokens = new ArrayList();
-        Lexico lexico = new Lexico(TextLanguage.getText()+" ");
-        
         DefaultTableModel model = new DefaultTableModel();
+        
+        //      LEXICO
+        Lexico lexico = new Lexico(TextLanguage.getText()+" ");
+        tokens = lexico.analisar();
+        
+        //      IMPRESSAO TABELA
+        model.setRowCount(0);
         model = (DefaultTableModel) TableLexico.getModel();
         
-        model.setRowCount(0);
-        
-        tokens = lexico.analisar();
         for(int i=0;i<tokens.size();i++){
             Token atual = tokens.get(i);
             model.addRow(new String[]{atual.getLexema(),atual.getToken(),String.valueOf(atual.getLinha()),String.valueOf(atual.getColuna())});
             
         }
         
+        //      SINTATICO
+        Sintatico sintatico = new Sintatico(tokens);
+        sintatico.analisar();
         
     }//GEN-LAST:event_jButton1ActionPerformed
 
@@ -248,6 +288,7 @@ public class IUPrincipal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTable TabelaSintatico;
     private javax.swing.JTable TableLexico;
     private javax.swing.JTextPane TextLanguage;
     private javax.swing.JButton jButton1;
@@ -256,8 +297,10 @@ public class IUPrincipal extends javax.swing.JFrame {
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
     private javax.swing.JSeparator jSeparator1;
     // End of variables declaration//GEN-END:variables
 }
--- src/compiler/Sintatico.java
+++ src/compiler/Sintatico.java
@@ -6,30 +6,381 @@
 package compiler;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
  * @author Guilherme
  */
 public class Sintatico {
-    private ArrayList<Token> tokens = new ArrayList();
+    private ArrayList<Token> tokens;
+    private ArrayList<Token> fila;
 
     public Sintatico() {
     }
     
     public Sintatico(ArrayList tokens) {
     this.tokens = tokens;
+    this.fila = tokens;
+    }
+    
+    public Token getNextToken(){
+        Token t = fila.get(0);
+        //fila.remove(0);
+        return t;
+    }
+    
+    public void remove(){
+        //Token t = fila.get(0);
+        fila.remove(0);
+        return ;
     }
     
     
+    public Boolean buscarToken(String token){
+        for(Token t: tokens){
+            if(t.getLexema().equals(token))
+                return true;
+        }
+        return false;
+    }
+    
+    public void analisar(){
+        
+        Token token = getNextToken();
+        if(token.getLexema().equals("program")){
+            remove();
+            I();
+            token = getNextToken();
+            if(token.getLexema().equals(";")){
+                remove();
+                A();
+            }else{
+                System.out.println("Erro falta de ;");
+                A();
+            }
+        }else{
+            System.out.println("Erro falta o token program");
+            I();
+            token = getNextToken();
+            if(token.getLexema().equals(";")){
+                remove();
+                A();
+            }else{
+                System.out.println("Erro falta de ;");
+                A();
+            }
+        }
+    }
+    
+
+    public void I(){
+        Token token = getNextToken();
+
+        if(token.getToken().equals("IDENTIFICADOR")){
+            remove();
+            return;
+        }
+        else System.out.println("Erro falta de identificador");
+    }
+    
+    public void A(){
+        B();
+        C();
+        D();
+    }
+    
+    public void B(){
+        Token token = getNextToken();
+        if(token.getLexema().equals("int") || token.getLexema().equals("real") || token.getLexema().equals("boolean") || token.getLexema().equals("char")){
+            remove();
+            I();
+            E();
+            B();
+        }//tratar erro
+        return;
+    }
+    
+    public void E(){
+        Token token = getNextToken();
+        if(token.getLexema().equals(",")){
+            remove();
+            I();
+            E();
+        }//tratar erro
+    }
+    
+    public void C(){
+        Token token = getNextToken();
+        if(token.getLexema().equals("procedure")){
+            remove();
+            I();
+            F();
+            token = getNextToken();
+            if(token.getLexema().equals(";")){
+                remove();
+                A();
+                token = getNextToken();
+                if(token.getLexema().equals(";")){
+                    remove();
+                    C();
+                }// tratar erro
+            }else{
+                System.out.println("Erro, falta ;");
+            }
+        }
+    }
+    
+    public void F(){
+        Token token = getNextToken();
+        if(token.getLexema().equals("(")){
+            remove();
+            G();
+            token = getNextToken();
+            if(token.getLexema().equals(")")){
+                remove();
+                return;
+            }
+        }
+    }
+    
+    public void G(){
+        Token token = getNextToken();
+        if(token.getLexema().equals("[")){
+            remove();
+            token = getNextToken();
+            if(token.getLexema().equals("var")){
+                remove();
+                token = getNextToken();
+                if(token.getLexema().equals("]")){
+                    remove();
+                    I();
+                    E();
+                    token = getNextToken();
+                    if(token.getLexema().equals(":")){
+                        remove();
+                        H();
+                        K();
+                    }
+                }
+            }
+        }
+    }
+    
+    public void H(){
+        Token token = getNextToken();
+        if(token.getLexema().equals("int")||token.getLexema().equals("real")||token.getLexema().equals("boolean")||token.getLexema().equals("char")){
+            remove();
+        }
+    }
+    
+    public void K(){
+        Token token = getNextToken();
+        if(token.getLexema().equals(";")){
+            remove();
+            G();
+        }
+    }
+    
+    public void D(){
+        Token token = getNextToken();
+        if(token.getLexema().equals("begin")){
+            remove();
+            L();
+            token = getNextToken();
+            if(token.getLexema().equals("end")){
+                remove();
+                return;
+            }
+        }
+    }
+    
+    public void L(){
+        Token token = getNextToken();
+        if(token.getLexema().equals("if")){
+            remove();
+            P();
+            L();
+            Q();
+            R();
+        }else if(token.getLexema().equals("while")){
+            remove();
+            P();
+            token = getNextToken();
+            if(token.getLexema().equals("do")){
+                remove();
+            }else System.out.println("Erro falta do");
+            L();
+            R();
+        }else if(token.getLexema().equals("begin")){
+            remove();
+            L();
+            R();
+            token = getNextToken();
+            if(token.getLexema().equals("end")){
+                remove();
+            }else System.out.println("Erro falta end");
+            R();
+        }else{//tratar erro
+            I();
+            M();
+            R();
+            }
+    }
+    
+    public void M(){
+        Token token = getNextToken();
+        if(token.getLexema().equals("(")){
+            remove();
+            M();
+            token = getNextToken();
+            if(token.getLexema().equals(")")){
+                remove();
+            }
+        }else{
+            O();
+            if(token.getLexema().equals(":")){
+                remove();
+                token = getNextToken();
+                if(token.getLexema().equals("=")){
+                    remove();
+                    P();
+                }
+            }
+        }
+    }
+    
+    public void O(){
+        P();
+    }
+    
+    public void Q(){
+        Token token = getNextToken();
+        if(token.getLexema().equals("else")){
+            remove();
+            L();
+        }
+    }
+    
+    public void R(){
+        Token token = getNextToken();
+        if(token.getLexema().equals(";")){
+            remove();
+            L();
+        }
+    }
+    
+    public void N(){
+        P();
+        J();
+    }
+    
+    public void J(){
+        Token token = getNextToken();
+        if(token.getLexema().equals(",")){
+            remove();
+            N();
+        }
+    }
+    
+    public void P(){
+        T();
+        U();
+    }
+    
+    public void T(){
+        Token token = getNextToken();
+        if(token.getLexema().equals("+") || token.getLexema().equals("-")){
+            remove();
+        }
+    }
+    
+    public void U(){
+        Token token = getNextToken();
+        if(token.getLexema().equals("(")){
+            remove();
+            P();
+            if(token.getLexema().equals(")")){
+                remove();
+            }
+            V();
+            X();
+            Y();
+            PP();
+        }else if(token.getLexema().equals("not")){
+            remove();
+            U();
+            V();
+            X();
+            Y();
+            PP();
+        }else if(token.getToken().equals("INTEIRO") || token.getToken().equals("REAL")){
+            Z();//gambiarra
+            V();
+            X();
+            Y();
+            PP();
+        }else{
+            I();
+            O();
+            V();
+            X();
+            Y();
+            PP();
+        }
+    }
+    
+    public void Z(){
+        Token token = getNextToken();
+        if(token.getToken().equals("INTEIRO") || token.getToken().equals("REAL")){
+            remove();
+            W();
+        }
+    }
+    
+    public void W(){
+        Token token = getNextToken();
+        if(token.getLexema().equals(".")){
+            remove();
+            Z();
+        }
+        Z();
+    }
+    
+    public void V(){
+        Token token = getNextToken();
+        if(token.getToken().equals("*") || token.getToken().equals("div") || token.getToken().equals("and")){
+            remove();
+            U();
+        }
+    }
+    
+    public void X(){
+        Token token = getNextToken();
+        if(token.getToken().equals("+") || token.getToken().equals("-") || token.getToken().equals("or")){
+            remove();
+            U();
+        }
+    }
+    
+    public void Y(){
+        Token token = getNextToken();
+        if(token.getToken().equals("=")||token.getToken().equals("<>")||token.getToken().equals("<")
+        ||token.getToken().equals("<=")||token.getToken().equals(">")||token.getToken().equals(">=")){
+            remove();
+            U();
+        }
+    }
+    
+    public void PP(){
+        Token token = getNextToken();
+        if(token.getToken().equals(",")){
+            remove();
+            P();
+        }
+    }
     
     
-    
-    
-    
-    
-    
-    
-    
-    
+
 }
